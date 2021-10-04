9dcdba7d73f8a50bf4d7c68552d659447e69589e
jareur <jm@euro-linux.com>
2021-10-04 22:14:28 +0200

    random fix

--- ctree.c
+++ ctree.c
@@ -8,11 +8,10 @@ int main() {
    system("clear");
    for (i=0; i<n; i++) {
        for (j=0; j < s; j++) printf(" ");
-       for (j=0; j < 2*i-1; j++) {
-           if ((rand() % 10) == 1) { printf("O"); }
-           else { printf("*"); }
-       }
-       s--;
-       printf("\n");
+       for (j=0; j < 2*i-1; j++) 
+           printf("%c", ((rand()%10) == 1) ? 
+           'O':'*'); s--; 
+       printf("\n"); 
    }
 }
+
